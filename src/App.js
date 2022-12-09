939a196cf28cb3ed73fae61e4c4db598afed8bb0
Marcos Moraes <contato@marcosamoraes.com>
2022-12-08 23:21:02 -0300

    components and pages created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Success from './pages/Success';
+import Seats from './pages/Seats';
+import Sessions from './pages/Sessions';
+import Home from './pages/Home';
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={Home}></Route>
+        <Route path="/sessoes/:id" element={Sessions}></Route>
+        <Route path="/assentos/:id" element={Seats}></Route>
+        <Route path="/sucesso" element={Success}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
