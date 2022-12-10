ef7527d4d4e0d1fd5fa27b34de2b33e7c563e36b
Marcos Moraes <contato@marcosamoraes.com>
2022-12-10 17:15:03 -0300

    fix for automatic tests

--- src/components/Seat/Seat.js
+++ src/components/Seat/Seat.js
@@ -1,4 +1,4 @@
-import { Button } from "./Seat.styled";
+import { Button, ButtonSelected, ButtonUnavailable } from "./Seat.styled";
 
 export default function Seat(props) {
   const isSelected = (id) => {
@@ -8,31 +8,45 @@ export default function Seat(props) {
   const selectSeat = (id, index) => {
     if (!isSelected(id)) {
       props.setSelectedSeats((selectedSeats) => [...selectedSeats, id]);
-      props.setSelectedSeatsIndex((selectedSeatsIndex) => [...selectedSeatsIndex, index]);
+      props.setSelectedSeatsIndex((selectedSeatsIndex) => [
+        ...selectedSeatsIndex,
+        index,
+      ]);
     } else {
       props.setSelectedSeats(props.selectedSeats.filter((seat) => seat !== id));
-      props.setSelectedSeatsIndex(props.selectedSeatsIndex.filter((seatIndex) => seatIndex !== index));
+      props.setSelectedSeatsIndex(
+        props.selectedSeatsIndex.filter((seatIndex) => seatIndex !== index)
+      );
     }
   };
 
   return (
     <>
       {props.seat.isAvailable ? (
-        <Button
-          className={isSelected(props.seat.id) ? "selected" : null}
-          onClick={() => selectSeat(props.seat.id, props.index+1)}
-          data-test="seat"
-        >
-          {String(props.index + 1).padStart(2, "0")}
-        </Button>
+        <>
+          {isSelected(props.seat.id) ? (
+            <ButtonSelected
+              onClick={() => selectSeat(props.seat.id, props.index + 1)}
+              data-test="seat"
+            >
+              {String(props.index + 1).padStart(2, "0")}
+            </ButtonSelected>
+          ) : (
+            <Button
+              onClick={() => selectSeat(props.seat.id, props.index + 1)}
+              data-test="seat"
+            >
+              {String(props.index + 1).padStart(2, "0")}
+            </Button>
+          )}
+        </>
       ) : (
-        <Button
-          className={"unavailable"}
-          onClick={() => alert('Esse assento não está disponível')}
+        <ButtonUnavailable
+          onClick={() => alert("Esse assento não está disponível")}
           data-test="seat"
         >
           {String(props.index + 1).padStart(2, "0")}
-        </Button>
+        </ButtonUnavailable>
       )}
     </>
   );
--- src/components/Seat/Seat.styled.js
+++ src/components/Seat/Seat.styled.js
@@ -14,14 +14,35 @@ export const Button = styled.button`
   padding: 0;
   font-size: 11px;
   margin-bottom: 18px;
+`
+export const ButtonUnavailable = styled.button`
+  background-color: #FBE192;
+  border: 1px solid #F7C52B;
+  border-radius: 12px;
+  margin-right: 7px;
+  box-sizing: border-box;
+  height: 25px;
+  width: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  font-size: 11px;
+  margin-bottom: 18px;
+`
 
-  &.unavailable {
-    background-color: #FBE192;
-    border-color: #F7C52B;
-  }
-
-  &.selected {
-    background-color: #1AAE9E;
-    border-color: #0E7D71;
-  }
+export const ButtonSelected = styled.button`
+  background-color: #1AAE9E;
+  border: 1px solid #0E7D71;
+  border-radius: 12px;
+  margin-right: 7px;
+  box-sizing: border-box;
+  height: 25px;
+  width: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  font-size: 11px;
+  margin-bottom: 18px;
 `
